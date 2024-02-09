e43af02cb07d2a20b248398003b803a15ad6b47d
Moritz Schmolke <moritz.schmolke@posteo.de>
2024-02-09 12:00:07 +0100

    slightly better

--- arduino/Distel/Distel.ino
+++ arduino/Distel/Distel.ino
@@ -47,12 +47,7 @@ byte adress;
 int enc0CC, enc1CC, enc2CC, enc3CC, enc4CC, enc5CC, enc6CC, enc7CC;
 int keyboardStatusCC, keyboardCodeCC;
 
-void handleNoteOn(byte inChannel, byte inNote, byte inVelocity) {
-    analogWrite(PIN_PC5, inVelocity * 8);
-  }
-void handleNoteOff(byte inChannel, byte inNote, byte inVelocity) {
-    digitalWrite(PIN_PC5, LOW);
-  }
+
 
 void setup() {
   pinMode(PIN_PD4, INPUT_PULLUP);
@@ -71,8 +66,7 @@ void setup() {
 
   MIDI.begin(adress);
   MIDI.setThruFilterMode(midi::Thru::DifferentChannel); // Different Channel
-  MIDI.setHandleNoteOn(handleNoteOn);
-  MIDI.setHandleNoteOff(handleNoteOff);
+
   keyboard.begin(DataPin, IRQPin);
   keyboard.setNoRepeat(1);
   keyboard.setNoBreak(1);
@@ -83,30 +77,51 @@ void setup() {
 int result = 0;
 void loop() {
 
-
   if (digitalRead(PIN_PD4)) {
+    
     result = en0.readAndReset();
     if (result != 0) { MIDI.sendControlChange(0, 64 - result, adress); }
+    readMIDI();
+
     result = en1.readAndReset();
     if (result != 0) { MIDI.sendControlChange(1, 64 - result, adress); }
+    readMIDI();
+    
     result = en2.readAndReset();
     if (result != 0) { MIDI.sendControlChange(2, 64 - result, adress); }
+     readMIDI();
+
+    
     result = en3.readAndReset();
     if (result != 0) { MIDI.sendControlChange(3, 64 - result, adress); }
+    readMIDI();
+
+    
   } else {
     result = en0.readAndReset();
     if (result != 0) { MIDI.sendControlChange(4, 64 - result, adress); }
+      readMIDI();
+
     result = en1.readAndReset();
     if (result != 0) { MIDI.sendControlChange(5, 64 - result, adress); }
+      readMIDI();
+
     result = en2.readAndReset();
     if (result != 0) { MIDI.sendControlChange(6, 64 - result, adress); }
+      readMIDI();
+
     result = en3.readAndReset();
     if (result != 0) { MIDI.sendControlChange(7, 64 - result, adress); }
+      readMIDI();
+
   }
 
+   readMIDI();
 
   if (keyboard.available()) {
     code = keyboard.read();
+    readMIDI();
+
     if (code > 0) {
      // MIDI.sendNoteOn(code >> 8, code & 0xFF, adress);
      MIDI.sendControlChange(keyboardStatusCC, code >> 8, adress);
@@ -114,6 +129,20 @@ void loop() {
     }
   }
 
- MIDI.read();
-    
+  readMIDI();
+
+}
+
+
+void readMIDI() {
+  if(MIDI.read()) {
+  switch(MIDI.getType()) {
+    case midi::NoteOn:
+      digitalWrite(PIN_PC5, HIGH);
+    break;
+    case midi::NoteOff:
+      digitalWrite(PIN_PC5, LOW);
+    break;
+    }
+  }
 }
