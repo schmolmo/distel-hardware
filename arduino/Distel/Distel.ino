43bbb0e43c2d5583fb84a2328b0e0864582fa742
Moritz Schmolke <moritz.schmolke@posteo.de>
2024-02-09 11:38:33 +0100

    not working

--- arduino/Distel/Distel.ino
+++ arduino/Distel/Distel.ino
@@ -42,46 +42,42 @@ Encoder en2(PIN_PB1, PIN_PB2);
 Encoder en3(PIN_PC0, PIN_PC1);
 
 uint16_t code;
-int adress = 0;
-int channel = 1;
+byte adress;
 
 int enc0CC, enc1CC, enc2CC, enc3CC, enc4CC, enc5CC, enc6CC, enc7CC;
 int keyboardStatusCC, keyboardCodeCC;
 
 void handleNoteOn(byte inChannel, byte inNote, byte inVelocity) {
-  if(inNote == adress) {
     analogWrite(PIN_PC5, inVelocity * 8);
   }
+void handleNoteOff(byte inChannel, byte inNote, byte inVelocity) {
+    digitalWrite(PIN_PC5, LOW);
   }
 
 void setup() {
-  MIDI.begin(0);
-  MIDI.setHandleNoteOn(handleNoteOn);
-  keyboard.begin(DataPin, IRQPin);
-  keyboard.setNoRepeat(1);
-  keyboard.setNoBreak(1);
   pinMode(PIN_PD4, INPUT_PULLUP);
   pinMode(DIP0, INPUT_PULLUP);
   pinMode(DIP1, INPUT_PULLUP);
   pinMode(DIP2, INPUT_PULLUP);
   pinMode(PIN_PC5, OUTPUT);
-
-
   bitWrite(adress, 0, digitalRead(DIP0));
   bitWrite(adress, 1, digitalRead(DIP1));
   bitWrite(adress, 2, digitalRead(DIP2));
+  bitWrite(adress, 3, LOW);
+  bitWrite(adress, 4, LOW);
+  bitWrite(adress, 5, LOW);
+  bitWrite(adress, 6, LOW);
+  bitWrite(adress, 7, LOW);
 
-  enc0CC = 0 + (12 * adress);
-  enc1CC = 1 + (12 * adress);
-  enc2CC = 2 + (12 * adress);
-  enc3CC = 3 + (12 * adress);
-  enc4CC = 4 + (12 * adress);
-  enc5CC = 5 + (12 * adress);
-  enc6CC = 6 + (12 * adress);
-  enc7CC = 7 + (12 * adress);
-
-  keyboardStatusCC = 8 + (12 * adress);
-  keyboardCodeCC = 9 + (12 * adress);
+  MIDI.begin(adress);
+  MIDI.setThruFilterMode(midi::Thru::DifferentChannel); // Different Channel
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  keyboard.begin(DataPin, IRQPin);
+  keyboard.setNoRepeat(1);
+  keyboard.setNoBreak(1);
+  keyboardStatusCC = 8;
+  keyboardCodeCC = 9;
 }
 
 int result = 0;
@@ -90,30 +86,31 @@ void loop() {
 
   if (digitalRead(PIN_PD4)) {
     result = en0.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc0CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(0, 64 - result, adress); }
     result = en1.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc1CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(1, 64 - result, adress); }
     result = en2.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc2CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(2, 64 - result, adress); }
     result = en3.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc3CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(3, 64 - result, adress); }
   } else {
     result = en0.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc4CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(4, 64 - result, adress); }
     result = en1.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc5CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(5, 64 - result, adress); }
     result = en2.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc6CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(6, 64 - result, adress); }
     result = en3.readAndReset();
-    if (result != 0) { MIDI.sendControlChange(enc7CC, result + 64, channel); }
+    if (result != 0) { MIDI.sendControlChange(7, 64 - result, adress); }
   }
 
 
   if (keyboard.available()) {
     code = keyboard.read();
     if (code > 0) {
-      MIDI.sendControlChange(keyboardStatusCC, code >> 8, 1);
-      MIDI.sendControlChange(keyboardCodeCC, code & 0xFF, 1);
+     // MIDI.sendNoteOn(code >> 8, code & 0xFF, adress);
+     MIDI.sendControlChange(keyboardStatusCC, code >> 8, adress);
+      MIDI.sendControlChange(keyboardCodeCC, code & 0xFF, adress);
     }
   }
 
